e5c7d697cfa19f81db406856019fbf39b80e60dd
enm2071 <enm2071@gmail.com>
2021-11-26 14:29:17 -0400

    Seccion de beneficios completada en un 80%

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-       // SCROLL
-       $(window).on("scroll", function () {
+    // SCROLL
+    $(window).on("scroll", function () {
         if ($(this).scrollTop() > 90) {
             $(".navbar").addClass("navbar-shrink");
-        }else{
+        } else {
             $(".navbar").removeClass("navbar-shrink");
         }
     })
@@ -13,32 +13,32 @@ $(document).ready(function () {
     $(".video-play-btn, .video-popup").on("click", function () {
         if ($(".video-popup").hasClass("open")) {
             $(".video-popup").removeClass("open");
-            $("#player-1").attr("src","")
+            $("#player-1").attr("src", "")
         } else {
             $(".video-popup").addClass("open");
             if ($("#player-1").attr("src") == "") {
-                $("#player-1").attr("src","videoSrc")                
+                $("#player-1").attr("src", "videoSrc")
             }
         }
     })
 
     //CAROUSEL FEATURES
     $('.features-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive : {
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
             // breakpoint from 0 up
-            0 : {
-                items : 1,
+            0: {
+                items: 1,
             },
             // breakpoint from 480 up
-            600 : {
-                items : 2,
+            600: {
+                items: 2,
             },
             // breakpoint from 768 up
-            1000 : {
-                items : 3,
+            1000: {
+                items: 3,
             }
         }
     });
@@ -56,13 +56,35 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
-document.getElementById("toggle").addEventListener("click", function () {
-
-    var x = document.getElementById("collapsibleNavbar");
-    if (x.className === "collapse navbar-collapse") {   
-        x.classList.add("show");
-    } else {
-        x.classList.remove("show");
+const hover = (element, section) => {
+    if (section == 1) {
+        const element1 = document.getElementsByClassName('img-line-1-beneficios');
+        element1[0].setAttribute('src', 'img/linea-1-avion-beneficios-section-green.png');
+        const element2 = document.getElementsByClassName('img-line-2-beneficios');
+        element2[0].setAttribute('src', 'img/linea-2-avion-beneficios-section-green.png');
+        const element3 = document.getElementsByClassName('img-line-3-beneficios');
+        element3[0].setAttribute('src', 'img/linea-3-avion-beneficios-section-green.png');
+        const element4 = document.getElementsByClassName('img-avion-beneficios');
+        element4[0].setAttribute('src', 'img/avion-beneficios-section-green.png');
+    } else if (section == 2) {
+        const element1 = document.getElementsByClassName('book-beneficios');
+        element1[0].setAttribute('src', 'img/libro-beneficion-section-green.png');
     }
-});
+}
+
+const unhover = (element, section) => {
+    if (section == 1) {
+        const element1 = document.getElementsByClassName('img-line-1-beneficios');
+        element1[0].setAttribute('src', 'img/linea-1-avion-beneficios-section-white.png');
+        const element2 = document.getElementsByClassName('img-line-2-beneficios');
+        element2[0].setAttribute('src', 'img/linea-2-avion-beneficios-section-white.png');
+        const element3 = document.getElementsByClassName('img-line-3-beneficios');
+        element3[0].setAttribute('src', 'img/linea-3-avion-beneficios-section-white.png');
+        const element4 = document.getElementsByClassName('img-avion-beneficios');
+        element4[0].setAttribute('src', 'img/avion-beneficios-section-white.png');
+    } else if (section == 2) {
+        const element1 = document.getElementsByClassName('book-beneficios');
+        element1[0].setAttribute('src', 'img/libro-beneficion-section-white.png');
+    }
+}
 
